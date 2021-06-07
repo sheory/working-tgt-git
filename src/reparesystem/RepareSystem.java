139701790930f52631647247098c26e8813a5162
= <sheoryd@gmail.com>
2021-06-07 20:33:39 -0300

    adicionando comentarios sobre como utilizar o git+netbeans

--- src/reparesystem/RepareSystem.java
+++ src/reparesystem/RepareSystem.java
@@ -15,16 +15,30 @@ public class RepareSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("sheory");
+        System.out.println("sheory ");
         System.out.println("Hello Git");
         System.out.println("Emily ");
     }
 }
 
 /*
-1.clonar 
-2.fazer as edições necessarias
-3.commit adicionando mensagem curta e direta sobre suas atualizações
-4.Expandir codigo
+    ----Repositorio existente--------
+    1.clonar 
+    2.fazer as edições necessarias
+    3.commit adicionando mensagem curta e direta sobre suas atualizações
+    4.Expandir codigo
+*/
+/*
+    ------Repositorio novo-------
+    1.inicializar repositorio
+    2.commit -> atualizando seu repositorio local
+    3.expandir... -> enviando commit ao repositorio do github
+*/
 
+/*
+    obs: 
+    -Quando estiver trabalhando em conjunto em um repositorio, 
+    sempre dê um "extrair" antes de começar a programar para ter seu codigo atualizado
+    conforme está no repositorio do github
+    -Coloque mensagens curtas e diretas no commit, apenas explicando suas atualizações
 */
